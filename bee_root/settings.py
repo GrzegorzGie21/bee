8ee8afb24a78728fdc9f41fc29dc3bc9135c7eb4
GrzegorzGie21 <galazka.grzesiek@gmail.com>
2020-03-13 12:55:50 +0100

    Added django-crispy-forms
    Aplied cripsy forms in settings

--- bee_root/settings.py
+++ bee_root/settings.py
@@ -37,8 +37,14 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    #Third-party-apps
+    'crispy_forms',
     #My apps
     'car_fleet',
+    'customer',
+    'employee',
+    'product',
+    'order',
 ]
 
 MIDDLEWARE = [
@@ -123,3 +129,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
