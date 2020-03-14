2c288f283c02b15d05f93a44a8f443ba7933a60c
GrzegorzGie21 <galazka.grzesiek@gmail.com>
2020-03-14 15:58:07 +0100

    Created CRUD for model Mileage
    Created views, templates and added urls responsible for displaying info about Mileage.

--- car_fleet/urls.py
+++ car_fleet/urls.py
@@ -1,10 +1,17 @@
 from django.urls import path
-from .views import CarListView, CarCreateView, CarUpdateView, CarDeleteView, CarDetailView
+from .views import (CarListView, CarCreateView, CarUpdateView, CarDeleteView, CarDetailView, MileageListView,
+                    MileageCreateView, MileageUpdateView, MileageDeleteView, MileageDetailView)
 
 urlpatterns = [
     path('', CarListView.as_view(), name='car-list'),
-    path('add', CarCreateView.as_view(), name='add-car'),
-    path('<pk>', CarDetailView.as_view(), name='car-detail'),
-    path('update/<pk>', CarUpdateView.as_view(), name='update-car'),
-    path('delete/<pk>', CarDeleteView.as_view(), name='delete-car'),
+    path('add/', CarCreateView.as_view(), name='add-car'),
+    path('<pk>/', CarDetailView.as_view(), name='car-detail'),
+    path('update/<pk>/', CarUpdateView.as_view(), name='update-car'),
+    path('delete/<pk>/', CarDeleteView.as_view(), name='delete-car'),
+    path('mileage/list/', MileageListView.as_view(), name='mileage-list'),
+    path('mileage/add/', MileageCreateView.as_view(), name='add-mileage'),
+    path('mileage/<pk>/', MileageDetailView.as_view(), name='mileage-detail'),
+    path('mileage/update/<pk>/', MileageUpdateView.as_view(), name='update-mileage'),
+    path('mileage/delete/<pk>/', MileageDeleteView.as_view(), name='delete-mileage'),
+
 ]
--- car_fleet/views.py
+++ car_fleet/views.py
@@ -1,7 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.urls import reverse_lazy
 from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
-from .models import Car
+from .models import Car, Mileage
 
 # Create your views here.
 class CarListView(ListView):
@@ -12,6 +12,12 @@ class CarListView(ListView):
 class CarDetailView(DetailView):
     model = Car
     template_name = 'car_detail.html'
+
+    def get_object(self, queryset=None):
+        obj = super().get_object()
+        obj.odometer = obj.update_odometer()
+        obj.save()
+        return obj
 
 
 class CarCreateView(CreateView):
@@ -32,3 +38,45 @@ class CarDeleteView(DeleteView):
     model = Car
     template_name = 'car_delete.html'
     success_url = reverse_lazy('car-list')
+
+
+class MileageListView(ListView):
+    model = Mileage
+    template_name = 'mileage_list.html'
+
+
+class MileageDetailView(DetailView):
+    model = Mileage
+    template_name = 'mileage_detail.html'
+
+
+class MileageCreateView(CreateView):
+    model = Mileage
+    template_name = 'mileage_add.html'
+    fields = ['start_day_odometer', 'end_day_odometer', 'date', 'car']
+    success_url = reverse_lazy('mileage-list')
+
+    def form_valid(self, form):
+        self.object = form.save(commit=False)
+        self.object.distance = self.object.calculate_distance()
+        form.save()
+        return redirect(self.success_url)
+
+
+class MileageUpdateView(UpdateView):
+    model = Mileage
+    fields = ['start_day_odometer', 'end_day_odometer', 'date', 'car']
+    template_name = 'mileage_update.html'
+    success_url = reverse_lazy('mileage-list')
+
+    def form_valid(self, form):
+        self.object = form.save(commit=False)
+        self.object.distance = self.object.calculate_distance()
+        form.save()
+        return redirect(self.success_url)
+
+
+class MileageDeleteView(DeleteView):
+    model = Mileage
+    template_name = 'mileage_delete.html'
+    success_url = reverse_lazy('mileage-list')
