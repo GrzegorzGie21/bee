68ab71ef6b370e93661374f470b6e580d552d58b
GrzegorzGie21 <galazka.grzesiek@gmail.com>
2020-03-14 20:48:40 +0100

    Added two apps in settings
    Added project product app third-party-app cripsy forms.

--- bee_root/settings.py
+++ bee_root/settings.py
@@ -37,8 +37,11 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    #Third-party-apps
+    'crispy_forms',
     #My apps
     'car_fleet',
+    'product',
 ]
 
 MIDDLEWARE = [
@@ -123,3 +126,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
