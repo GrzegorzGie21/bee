40e3263168c7c3beb2622d1c1a35eb13d08772b4
GrzegorzGie21 <galazka.grzesiek@gmail.com>
2022-01-11 00:50:39 +0100

    feat: display current project version
    Added APP_VERSION variable in project settings and applied it in context processor

--- bee_root/project_context_processors.py
+++ bee_root/project_context_processors.py
@@ -1,6 +1,7 @@
 from django.utils import timezone
+from django.conf import settings
 
 def footer_cp(request):
     context = {'time': timezone.now(),
-               'version': '1.0'}
+               'version': settings.APP_VERSION}
     return context
