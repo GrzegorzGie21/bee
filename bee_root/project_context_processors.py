fa9b41e8988713da6d81c42a62e36598f232b4c2
GrzegorzGie21 <galazka.grzesiek@gmail.com>
2022-01-11 13:29:43 +0100

    fix: correcting misspelling method's name which gets the current project version

--- bee_root/project_context_processors.py
+++ bee_root/project_context_processors.py
@@ -4,7 +4,7 @@ from django.conf import settings
 import os
 
 
-def get_cuurent_git_version():
+def get_current_git_version():
     git_cmd = 'git describe --tags $(git rev-list --tags --max-count=1)'
     result = os.popen(git_cmd)
     version = result.read().strip()
@@ -13,5 +13,5 @@ def get_cuurent_git_version():
 
 def footer_cp(request):
     context = {'time': timezone.now(),
-               'version': get_cuurent_git_version()}
+               'version': get_current_git_version()}
     return context
