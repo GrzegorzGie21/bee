61effe9a2ba51f11b2b4ef38968e303e88bffec8
GrzegorzGie21 <galazka.grzesiek@gmail.com>
2022-01-11 13:22:14 +0100

    feat: automatically get the project current version
    Created function that run specific git command using os module.

--- bee_root/project_context_processors.py
+++ bee_root/project_context_processors.py
@@ -1,7 +1,17 @@
 from django.utils import timezone
 from django.conf import settings
 
+import os
+
+
+def get_cuurent_git_version():
+    git_cmd = 'git describe --tags $(git rev-list --tags --max-count=1)'
+    result = os.popen(git_cmd)
+    version = result.read().strip()
+    return version[1:]
+
+
 def footer_cp(request):
     context = {'time': timezone.now(),
-               'version': settings.APP_VERSION}
+               'version': get_cuurent_git_version()}
     return context
