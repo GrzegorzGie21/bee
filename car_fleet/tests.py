504c6ced53a7877f122d41486191a08f7e728e34
GrzegorzGie21 <galazka.grzesiek@gmail.com>
2022-01-21 20:26:45 +0100

    feat(car_fleet): write basic test for home page

--- car_fleet/tests.py
+++ car_fleet/tests.py
@@ -1,3 +1,29 @@
-from django.test import TestCase
+from django.test import TestCase, SimpleTestCase
+from django.shortcuts import reverse
+from django.urls import resolve
+from employee.views import index
+
 
 # Create your tests here.
+
+class HomePageTests(SimpleTestCase):
+
+    def setUp(self):
+        url = reverse('home')
+        self.response = self.client.get(url)
+
+    def test_home_page_status_code(self):
+        self.assertEqual(self.response.status_code, 200)
+
+    def test_home_page_contains_correct_content(self):
+        self.assertContains(self.response, 'Order and report app')
+
+    def test_home_page_does_not_contains_incorrect_content(self):
+        self.assertNotContains(self.response, 'Hello')
+
+    def test_home_page_use_correct_template(self):
+        self.assertTemplateUsed(self.response, 'home.html')
+
+    def test_home_page_url_resolve_home_page_view(self):
+        match = resolve('/')
+        self.assertEqual(match.func.__name__, index.__name__)
