3759c2ad123c40632f32781aabfabcb153d37c80
GrzegorzGie21 <galazka.grzesiek@gmail.com>
2022-01-23 12:00:27 +0100

    feat(car_fleet): add get_absolute_url method to car model
    Updated car_list template which is using get_absolute_url method from car object

--- car_fleet/models.py
+++ car_fleet/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.shortcuts import reverse
 
 
 # Create your models here.
@@ -21,6 +22,9 @@ class Car(models.Model):
 
     def __str__(self):
         return f'{self.manufacturer} {self.model} ({self.registration_number})'
+
+    def get_absolute_url(self):
+        return reverse('car:car-detail', kwargs={'pk': self.pk})
 
 
 class Mileage(models.Model):
